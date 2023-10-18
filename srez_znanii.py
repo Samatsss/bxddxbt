2b08053bd88df54950968f7b100bd3e5c4260c39
Samatsss <144174632+Samatsss@users.noreply.github.com>
2023-10-18 10:01:56 +0300

    rvz
    rvvx

---
+++ srez_znanii.py
@@ -0,0 +1,34 @@
+# задание 1
+#List = [None, None, 1, [], (), {}, None]
+#def de_none(List):
+#    print(List.replace('None','',3)
+#    return(List)
+#print(de_none(List))
+
+
+
+
+# задание 2
+
+#ref_list = [0, 1, 2, 3, 4, 5]
+#start =  4
+#num   = 40
+#rep   =  2
+#def list_insert(ref_list):
+#    if len(ref_list) < start:
+#        return -1
+#    ref_list.append(num, rep)
+#    return ref_list
+#print(list_insert(ref_list))
+
+
+
+
+# Задание 3
+
+#d = {'a': 1, 'b': None, 'c': [1, 2]}
+#k = 'a'
+#def get_elem(d, k):
+#    if k in d:
+#        return(d[k])
+#print(get_elem(d, k))
